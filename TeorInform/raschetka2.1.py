452c1aa7dc10d00e92b4cffd481cb2f6fc2e94f3
lozert <tudvasev62@gmail.com>
2025-01-03 18:13:30 +0500

    Исправление задания

--- TeorInform/raschetka2.1.py
+++ TeorInform/raschetka2.1.py
@@ -2,7 +2,7 @@ import numpy as np
 from entropy import H, HNderM
 
 
-n = 5
+n = 51
 
 PXY = np.array([[n/260,       1/14,       1/14],
        [2/7 - n/240, 0,          2/7 - n/250],
@@ -38,6 +38,6 @@ print(f"H(Y/X) = {HNderM(PXY, PYderX)}")
 
 
 print(f"\nH(X/y1) = {H(PXderY.T[0])}")
-print(f"H(X/y2) = {H(PXderY.T[1])}")
+print(f"H(Y/x2) = {H(PYderX[1])}")
 
 print(f"\nH(X; Y) = {HNderM(PXY, PXY)}")
